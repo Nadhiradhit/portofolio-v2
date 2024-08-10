2b09ba14adbc6f711ff3e2c9b367626a92447d52
Nadhiradhit <nadhiradhitya@gmail.com>
2024-08-10 15:00:15 +0700

    refractor: folder structure

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -3,6 +3,9 @@ import Link from "next/link";
 import Navbar from "@/components/navigation/navbar";
 import Divider from "@/components/divider/divider-line";
 import { MdOutlineWebAsset } from "react-icons/md";
+import { personalTask } from "@/lib/common/profile";
+import Image from "next/image";
+import paperImage from "@/public/assets/paper.jpg";
 
 export default function page() {
 	return (
@@ -35,9 +38,9 @@ export default function page() {
 						</Link>
 					</div>
 				</div>
-				<div className="w-auto p-10 absolute top-0">
+				<div className="w-full p-10 absolute top-0">
 					<div>
-						<p className="text-[2rem] font-semibold text-start">About Me</p>
+						<p className="text-[1.5rem] font-semibold text-start">About Me</p>
 						<div className="w-14 h-1 bg-[#5389cf] rounded-md"></div>
 						<div className="py-5 text-sm">
 							<p className="text-justify">
@@ -54,36 +57,28 @@ export default function page() {
 							</p>
 						</div>
 					</div>
-					<div className="">
-						<p className="text-[2rem] font-semibold text-start">
+					<div>
+						<p className="text-[1.5rem] font-semibold text-start">
 							What I&apos;m Doing
 						</p>
-						<div className="flex flex-wrap justify-center gap-4 pt-4">
-							<div className="w-[565px] bg-[#2d2e30] rounded-md flex items-center gap-5 p-5">
-								<MdOutlineWebAsset size={100} className="" />
-								<div className="w-2/3">
-									<p className="text-[1.5rem] font-semibold">Web Design</p>
-									<p className="text-sm font-light">
-										Lorem ipsum dolor sit amet consectetur adipisicing elit.
-										Tempora, excepturi molestias obcaecati quas qui perspiciatis
-										cumque sit, explicabo consequuntur, vero consequatur natus
-										laudantium velit. Reiciendis ipsum ab commodi architecto
-										nisi!
-									</p>
-								</div>
-							</div>
-							<div className="w-[565px] bg-[#2d2e30] rounded-md flex items-center gap-5 p-5">
-								<MdOutlineWebAsset size={100} />
-								<div className="w-2/3">
-									<p className="text-[1.5rem] font-semibold">Web Design</p>
-									<p className="text-sm font-light">
-										Lorem ipsum dolor sit amet consectetur adipisicing elit.
-										Tempora, excepturi molestias obcaecati quas qui perspiciatis
-										cumque sit, explicabo consequuntur, vero consequatur natus
-										laudantium velit. Reiciendis ipsum ab commodi architecto
-										nisi!
-									</p>
-								</div>
+						<div className="h-screen bg-cover bg-center bg-no-repeat">
+							<div className="grid grid-cols-2 gap-2 p-1.5 ">
+								{personalTask.map((item, index) => {
+									const IconType = item.icons;
+									return (
+										<div
+											className="w-full h-32 bg-[#2d2e30] rounded-md flex items-center justify-center gap-5 p-5"
+											key={index}>
+											<IconType size={65} className=" text-[#5389cf]" />
+											<div className="w-3/4">
+												<p className="text-[1.2rem] font-semibold text-justify">
+													{item.title}
+												</p>
+												<p className="text-sm font-light">{item.description}</p>
+											</div>
+										</div>
+									);
+								})}
 							</div>
 						</div>
 					</div>
