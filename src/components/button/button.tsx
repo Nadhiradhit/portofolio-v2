aead504ff32f8534042157dbafd3851d9910f7fb
Nadhiradhit <nadhiradhitya@gmail.com>
2024-09-04 11:53:59 +0700

    fix: props type in button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,18 +1,23 @@
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 import React from "react";
 
 interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
 	disabled?: boolean;
 	children?: React.ReactNode;
 	className?: string;
-	type?: "primary" | "secondary" | "submit" | "reset";
+	href: string;
+	text: string;
+	typeButton?: "primary" | "secondary" | "submit" | "reset";
 }
 
 export default function Button({
 	className,
 	disabled,
 	children,
-	type,
+	typeButton,
+	text,
+	href,
 	...props
 }: ButtonProps) {
 	return (
@@ -20,16 +25,17 @@ export default function Button({
 			className={cn(
 				"px-4 py-2 rounded-full hover:scale-105 transition transform ease-in-out",
 				{
-					"bg-white border border-blue-300": type === "primary",
+					"bg-white border border-blue-300": typeButton === "primary",
 					"cursor-not-allowed opacity-50 bg-gray-400 disabled:hover:scale-100":
 						disabled,
-					"bg-blue-400 text-white hover:bg-blue-300": type === "secondary",
+					"bg-blue-400 text-white hover:bg-blue-300":
+						typeButton === "secondary",
 				},
 				className
 			)}
 			disabled={disabled}
 			{...props}>
-			{children}
+			<Link href={href}>{text}</Link>
 		</button>
 	);
 }
