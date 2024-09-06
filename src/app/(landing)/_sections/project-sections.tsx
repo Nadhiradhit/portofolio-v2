48cbe44dc64179b0f1d830752d0d2df8ad4adcd2
Nadhiradhit <nadhiradhitya@gmail.com>
2024-09-06 17:59:43 +0700

    fix: Fixing text color, Fixing button color

--- src/app/(landing)/_sections/project-sections.tsx
+++ src/app/(landing)/_sections/project-sections.tsx
@@ -14,10 +14,10 @@ export default function ProjectSections() {
 	const haveMoreProjects = projects.length > 4;
 	const maxProjects = projects.slice(0, 4);
 	return (
-		<section className="px-10 py-10 w-full ">
+		<section className="px-10 py-10 w-full">
 			<main className=" bg-[#D0E8F4] rounded-xl px-5 py-2">
 				<div className="py-6 flex justify-between">
-					<h3 className="text-black">My Projects</h3>
+					<h3 className="text-[#0F88BC] font-semibold">My Projects</h3>
 					{haveMoreProjects && (
 						<button className="w-56 h-10 border border-blue-300 rounded-full hover:scale-105 hover:bg-[#edf5ff] transition transform ease-in-out">
 							View More Projects
@@ -37,13 +37,15 @@ export default function ProjectSections() {
 										className="rounded-t-xl cursor-pointer hover:scale-90 hover:rounded-lg transition transform ease-in-out "
 									/>
 								</CardImage>
-								<CardTitle>{project.name}</CardTitle>
-								<CardDescription>{project.description}</CardDescription>
+								<CardTitle className="text-[#0F88BC]">{project.name}</CardTitle>
+								<CardDescription className="text-[#0F88BC]">
+									{project.description}
+								</CardDescription>
 								<CardFooter>
 									<Button
 										href={project.url}
 										text="View Project"
-										className="text-white bg-blue-300 w-full rounded-md hover:bg-blue-600"
+										className="text-white bg-[#0F88BC] w-full rounded-md hover:bg-[#0a5e81]"
 									/>
 								</CardFooter>
 							</Card>
