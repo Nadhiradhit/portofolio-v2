b325fbd978f10cd44be00f69f9d633c0a8e206ba
Nadhiradhit <nadhiradhitya@gmail.com>
2024-09-06 17:49:32 +0700

    feat: Add project section and fixing some data project

---
+++ src/app/(landing)/_sections/project-sections.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Image from "next/image";
+import { projects } from "@/lib/common/project";
+import {
+	Card,
+	CardImage,
+	CardTitle,
+	CardDescription,
+	CardFooter,
+} from "@/components/card/card";
+import { Button } from "@/components/button/button";
+
+export default function ProjectSections() {
+	const haveMoreProjects = projects.length > 4;
+	const maxProjects = projects.slice(0, 4);
+	return (
+		<section className="px-10 py-10 w-full ">
+			<main className=" bg-[#D0E8F4] rounded-xl px-5 py-2">
+				<div className="py-6 flex justify-between">
+					<h3 className="text-black">My Projects</h3>
+					{haveMoreProjects && (
+						<button className="w-56 h-10 border border-blue-300 rounded-full hover:scale-105 hover:bg-[#edf5ff] transition transform ease-in-out">
+							View More Projects
+						</button>
+					)}
+				</div>
+				<div className="grid grid-cols-1 md:grid-cols-2 lg:flex  gap-4 pb-6 ">
+					{maxProjects.map((project, index) => {
+						return (
+							<Card key={index}>
+								<CardImage>
+									<Image
+										src={project.image}
+										alt={project.name}
+										width={500}
+										height={300}
+										className="rounded-t-xl cursor-pointer hover:scale-90 hover:rounded-lg transition transform ease-in-out "
+									/>
+								</CardImage>
+								<CardTitle>{project.name}</CardTitle>
+								<CardDescription>{project.description}</CardDescription>
+								<CardFooter>
+									<Button
+										href={project.url}
+										text="View Project"
+										className="text-white bg-blue-300 w-full rounded-md hover:bg-blue-600"
+									/>
+								</CardFooter>
+							</Card>
+						);
+					})}
+				</div>
+			</main>
+		</section>
+	);
+}
--- src/lib/common/project.ts
+++ src/lib/common/project.ts
@@ -1,23 +1,61 @@
-import paperImage from "../../public/assets/paper.jpg";
+import { RiNextjsLine, RiTailwindCssLine } from "react-icons/ri";
+import paperImage from "@/public/assets/paper.jpg";
+import { SiTypescript } from "react-icons/si";
+
+import AudioBookImage from "@/public/images/audiobookImg.png";
+import NewsInImage from "@/public/images/newsinImg.png";
+import EventifyImage from "@/public/images/eventifyImg.png";
+import PersonalImage from "@/public/images/personalImg.png";
 
 const projects = [
 	{
-		name: "Portofolio",
-		description: "My Portofolio Website",
-		image: paperImage,
-		url: "https://portofolio-nadhir.vercel.app/",
+		name: "Portofolio Website",
+		description:
+			"Web-base an application as a portofolio website that's include an experince skill.",
+		image: PersonalImage,
+		url: "https://porto-nadhir.vercel.app/",
+		language: [
+			{
+				name: "Next.js",
+				icon: RiNextjsLine,
+			},
+			{
+				name: "Tailwind",
+				icon: RiTailwindCssLine,
+			},
+			{
+				name: "TypeScript",
+				icon: SiTypescript,
+			},
+		],
 	},
 	{
-		name: "Portofolio",
-		description: "My Portofolio Website",
-		image: paperImage,
-		url: "https://portofolio-nadhir.vercel.app/",
+		name: "Eventify",
+		description:
+			"Web base an application as an event management system includes catering, videography, photography, makeup and more.",
+		image: EventifyImage,
+		url: "https://github.com/kelompok-1-Gitsid/Eventify",
 	},
 	{
-		name: "Portofolio",
-		description: "My Portofolio Website",
+		name: "AudioBook Polimedia",
+		description:
+			"Web base an application as an audio book management system for reading while listening to stories.",
+		image: AudioBookImage,
+		url: "https://github.com/asyarbre/audiobook-web",
+	},
+	{
+		name: "NewsIn",
+		description:
+			"Simple web base an application as a web to input the latest news with a simple CRUD system.",
+		image: NewsInImage,
+		url: "https://github.com/Nadhiradhit/newsin-app",
+	},
+	{
+		name: "Pilih",
+		description:
+			"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi, quibusdam omnis! Error, blanditiis deleniti, unde laboriosam ex quas, vitae soluta dolore eos minima porro dolor neque reprehenderit ea omnis necessitatibus.",
 		image: paperImage,
-		url: "https://portofolio-nadhir.vercel.app/",
+		url: "https://porto-nadhir.vercel.app/",
 	},
 ];
 
