c3687faf264ef110860ba0ca4469376785a7ec26
Nadhiradhit <nadhiradhitya@gmail.com>
2024-09-09 12:26:26 +0700

    feat: Add footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+	return (
+		<footer className="bg-[#fcfcfc] text-center py-8 dark:text-slate-100">
+			<p>Thanks for visiting my portfolio.</p>
+			<small>&copy; 2024 Nadhir Adhitya Zhalifunnas. All rights reserved</small>
+		</footer>
+	);
+}
