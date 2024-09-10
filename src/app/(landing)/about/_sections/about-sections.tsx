eb374ef77089344e7d1c00b5106bf868745856ab
Nadhiradhit <nadhiradhitya@gmail.com>
2024-09-10 19:56:58 +0700

    feat: about pages

---
+++ src/app/(landing)/about/_sections/about-sections.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+
+import ImageHeader from "../partials/image-header";
+export default function AboutSection() {
+	return (
+		<section className="px-10">
+			<ImageHeader />
+			<div className="pt-16 justify-center items-center">
+				<h2 className={cn("font-bold")}>About Me</h2>
+				<article className="pt-4 ">
+					<p>
+						My name Nadhir Adhitya Zhalifunnas. Im a Fullstack Developer and
+						Frontend Developer with 4 years of experience. I enjoy building
+						websites with a powerfull design, i also enjoy to design a websites.
+						Right now im focused at working in Next.js and Express.js.
+					</p>
+					<p>
+						Experienced with Full Stack Web Developer and Frontend Web Developer
+						, Skilled in a wide range of technologies including HTML, CSS,
+						Javascript, PHP, Laravel, Next.js, Node.js, Tailwindcss,
+						Bootstrapcss. My favorite part of programming is design website, and
+						problem-solving aspect. Who has some value to build a creative and
+						interactive Website. Able to effectively self-manage during
+						independent projects, and weel to collaborate in a team. I am always
+						looking to learn new technologies. I am currently looking for a
+						full-time position as a Full-Stack Web Developer and Front-End Web
+						Developer.
+					</p>
+				</article>
+			</div>
+		</section>
+	);
+}
---
+++ src/app/(landing)/about/partials/image-header.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Image from "next/image";
+import { VelocityScroll } from "@/components/magicui/scroll-based-velocity";
+
+export default function ImageHeader() {
+	return (
+		<section className="w-full flex justify-center items-center">
+			<div className="absolute w-full h-full top-64">
+				<VelocityScroll
+					text="Frontend Developer"
+					default_velocity={2}
+					className="font-display text-center text-4xl font-bold tracking-[-0.02em] text-black drop-shadow-sm dark:text-white md:text-7xl md:leading-[5rem]"
+				/>
+			</div>
+			<Image
+				width={300}
+				height={500}
+				src={"/img/profil.jpg"}
+				alt="image"
+				className="rounded-lg relative bottom-16 grayscale hover:grayscale-0 transition-all ease-in-out cursor-pointer hover:scale-110"
+			/>
+		</section>
+	);
+}
